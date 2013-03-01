66f9df0c0e028cb1f480c51ef70f48238f88b4dd
lzhiheng@eng.ucsd.edu <lzhiheng@eng.ucsd.edu@bbb27050-87f0-787e-9b95-e590696d5685>
2013-03-01 04:56:53 +0000

    sorry= =

--- trunk/src/db/ClassSection.java
+++ trunk/src/db/ClassSection.java
@@ -1,11 +1,13 @@
 package db;
 
+import java.util.ArrayList;
+
 public class ClassSection
 {
     public String sectionId;
     public String facultyId;
     public int limit;
-    public Meeting[] meetingList; 
+    public ArrayList<Meeting> meetingList; 
 
     public ClassSection(String sectionId, String facultyId, int limit) {
         this.sectionId = sectionId;
@@ -13,6 +15,25 @@ public class ClassSection
         this.limit = limit;
     }
 
+    public ClassSection(String sectionId) {
+    	this.sectionId = sectionId;
+    	this.meetingList = new ArrayList<Meeting>();
+    }
+    
     public ClassSection(){
+    }
+    
+    public void addMeeting(Meeting m) {
+    	this.meetingList.add(m);
+    }
+    
+    public boolean conflictWith(ClassSection b) {
+    	for (Meeting m : this.meetingList) {
+    		for (Meeting n : b.meetingList) {
+    			if (m.conflictWith(n))
+    				return true;
+    		}
+    	}
+    	return false;
     }
 }
